e0f75c13aac4783e31a3aa1fa1047f8cddc8a513
sjk <lueky4300@naver.com>
2021-03-19 07:11:07 +0800

    content html ccustomize

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "지킬(jekyll) 로 포스팅하기",
+        "excerpt":"   블로그 설치 완료    이제 글만 작성하면 된다..  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/blog/start/",
+        "teaser": null
+      }]
